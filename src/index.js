d0541df9ca669b6f61a78b1d2a9999fe5c5bb587
forestsheep <forestsheep@163.com>
2021-06-09 17:20:38 +0800

    loop research

--- src/index.js
+++ src/index.js
@@ -9,7 +9,8 @@ console.log("App started");
 const ay = [];
 ay.push(2);
 ay.push(3);
-ay.push({ app: 1 });
+// ay.push({ app: 1 });
+ay.app = 1;
 ay.push(() => {});
 
 console.warn("for let");
@@ -21,3 +22,20 @@ console.warn("forEach");
 ay.forEach((pp) => {
   console.log(pp);
 });
+
+console.warn("for in");
+for (const i in ay) {
+  console.log(i);
+  console.log(ay[i]);
+}
+
+console.warn("for of");
+for (const element of ay) {
+  console.log(element);
+}
+
+console.warn("object");
+console.log(ay);
+console.log(ay.app);
+console.log(ay.push);
+console.log(ay.slice);
